593c688114a65110c963f4db6faa8eb1aad7647f
clarin531 <clarinkyng@gmail.com>
2019-08-23 17:40:19 -0700

    add case study

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+$(document).ready(function(){
 
 $("#paragraph-btn").click(function(){
     $("#more-about-me").css('display',"block");
@@ -56,3 +57,73 @@ $(document).scroll(function() {
     $('.iphone-dropdown a').click(function(event){
    event.stopPropagation();
 });
+
+  //sliders//current position
+var pos = 0;
+//number of slides
+var totalSlides = $('#slider-wrap ul li').length;
+//get the slide width
+var sliderWidth = $('#slider-wrap').width();
+
+	/*****************
+	 BUILD THE SLIDER
+	*****************/
+	//set width to be 'x' times the number of slides
+	$('#slider-wrap ul#slider').width(sliderWidth*totalSlides);
+
+    //next slide
+	$('#next').click(function(){
+		slideRight();
+	});
+
+	//previous slide
+	$('#previous').click(function(){
+		slideLeft();
+	});
+
+  $('#slider-wrap').hover(
+  	  function(){ $(this).addClass('active'); clearInterval(autoSlider); },
+  	  function(){ $(this).removeClass('active');}
+  	);
+
+  function slideLeft(){
+  	pos--;
+  	if(pos==-1){ pos = totalSlides-1; }
+  	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos));
+  }
+  function slideRight(){
+	pos++;
+	if(pos==totalSlides){ pos = 0; }
+	$('#slider-wrap ul#slider').css('left', -(sliderWidth*pos));
+}
+});;
+
+//Slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
